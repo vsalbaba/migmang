7647c3540d75d6e08387feec6d5403fa5259f514
DarkTatka <darktatka@gmail.com>
2009-02-26 23:55:40 +0100

    vylepseni to_noted
    predtim bralo to_noted 2 parametry - x, y. Casto se ale stalo, ze x, y byly v poli [x, y], a zbytecne se z nej vybirali pred volanim funkce, coz skaredilo kod.
    
    Signed-off-by: DarkTatka <darktatka@gmail.com>

--- lib/enhancements/mig_mang_board_helper.rb
+++ lib/enhancements/mig_mang_board_helper.rb
@@ -2,8 +2,15 @@ module MigMangBoardHelper
 	def from_noted(string)
 		[string[0]-97, string[1].chr.to_i-1]
   end
-
-  def to_noted(x,y)
-		"#{(97+x).chr}#{y+1}"
+=begin rdoc
+takes 1 or 2 arguments - first argument is x coord or array [x, y] - second is optional y coord.
+=end
+  def to_noted(x,y = nil)
+		return "#{(97+x).chr}#{y+1}" if y
+		return "#{(97+x[0]).chr}#{x[1]+1}"
+  end
+  
+  def empty_neighbours_for(position)
+    from_noted(position).neighbours(0, 9).delete_if {&:full?}
   end
 end
