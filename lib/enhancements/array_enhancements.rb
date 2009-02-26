be51e6de7a9b30342ff3bbd3640094845fefc704
DarkTatka <darktatka@gmail.com>
2009-02-26 23:53:38 +0100

    oprava only_those_on_board
    Signed-off-by: DarkTatka <darktatka@gmail.com>

--- lib/enhancements/array_enhancements.rb
+++ lib/enhancements/array_enhancements.rb
@@ -7,14 +7,16 @@ class Array
   def left;  copy = self.dup; copy[0] -= 1; copy end
 
   def right; copy = self.dup; copy[0] += 1; copy end
+
   # vyhazi prvky ktere nemaji vsechny souradnice mezi min a max.
   def only_those_on_board(min,max)
     self.delete_if do |position| 
-                     position.all? {|coord| coord.between?(min,max) }
+                     position.any? {|coord| !coord.between?(min,max) }
                    end
+    self
   end
   
-  def neighbours
-    [ self.up, self.down, self.left, self.right ].only_those_on_board
+  def neighbours(min, max)
+    [ self.up, self.down, self.left, self.right ].only_those_on_board(0, 9)
   end
 end
