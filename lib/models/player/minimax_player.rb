4e5220033a19a190e06a4f26f03d622ca9ae4809
DarkTatka <darktatka@gmail.com>
2009-03-09 16:37:29 +0100

    pridan pseudokod

--- lib/models/player/minimax_player.rb
+++ lib/models/player/minimax_player.rb
@@ -4,7 +4,17 @@ class MinimaxPlayer < AbstractPlayer
   def pick_move(moves)
     moves[rand(moves.size)]
   end
-  
+=begin rdoc
+pseudocode
+function minimax(node, depth)
+    if node is a terminal node or depth = 0
+        return the heuristic value of node
+    else
+        let α := -∞
+        foreach child of node                       { evaluation is identical for both players }
+            let α := max(α, -minimax(child, depth-1))
+        return α
+=end
   def minimax(board, depth)
     if board.ended? or depth == 0
       return score(board)
