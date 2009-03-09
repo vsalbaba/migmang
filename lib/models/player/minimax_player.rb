aea04be74c8f7fbcf7922eaa5517b3092b5b52de
DarkTatka <darktatka@gmail.com>
2009-03-09 20:00:43 +0100

    vice pseudo, ohodnocovaci funkce

--- lib/models/player/minimax_player.rb
+++ lib/models/player/minimax_player.rb
@@ -1,12 +1,13 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), "abstract_player")
-
+WHITE = 1
+BLACK = 2
 class MinimaxPlayer < AbstractPlayer
   def pick_move(moves)
     moves[rand(moves.size)]
   end
 =begin rdoc
-pseudocode
-function minimax(node, depth)
+pseudocode:
+ function minimax(node, depth)
     if node is a terminal node or depth = 0
         return the heuristic value of node
     else
@@ -19,14 +20,28 @@ function minimax(node, depth)
     if board.ended? or depth == 0
       return score(board)
     else
-      leafs = board.moves_for(board.on_move).map{|move|
+      nodes = board.moves_for(board.on_move).map{|move|
         board.apply_move(move)
       }
       alpha = -100
-      for leaf in leafs
-        alpha = [alpha, -miminax(leaf, depth - 1)].max
+      for child in nodes
+        alpha = [alpha, -miminax(child, depth - 1)].max
       end
       return alpha
+    end
+  end
+  
+  def score(board)
+    if board.ended?
+      board.on_move == board.winner ? return 100 : return -100
+    else
+      figures = 0
+      board.board.each_with_keys do |x, y, key|
+        if key == board.on_move
+          figures += 1
+        end
+      end
+      return figures
     end
   end
 end
