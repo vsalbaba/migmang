8dd71d61b2e6f4b23cc3088c0275b56de946f7aa
DarkTatka <darktatka@gmail.com>
2009-04-18 16:48:34 +0200

    upravy kodu pro vetsi citelnost
    x a y nejsou prehledne nazvy promennych

--- lib/models/board.rb
+++ lib/models/board.rb
@@ -1,40 +1,40 @@
 class Board
 
-  def initialize(x, y)
-    @x = x
-    @y = y
-    # je potreba volat s blokem, volani Array.new(@x, Array.new(@y, 0)) 
+  def initialize(x_size, y_size)
+    @x_size = x_size
+    @y_size = y_size
+    # je potreba volat s blokem, volani Array.new(@x, Array.new(@y_size, 0)) 
     # by byly vsechny polozky jen odkazy na to stejne pole
-    @inner_representation = Array.new(@x) {|index| Array.new(@y, 0)}
+    @inner_representation = Array.new(@x_size) {|index| Array.new(@y_size, 0)}
   end
 
   def each_with_keys(&block)
-    width, height = @x, @y
-    for x in 0..width-1
-      for y in 0..height-1
-        yield x,y,self[x,y]
+    width, height = @x_size, @y_size
+    for x_coord in 0..width-1
+      for y_coord in 0..height-1
+        yield x_coord,y_coord,self[x_coord,y_coord]
       end
     end
   end
 
-  def [](x, y)
-    raise "OutOfIndexError" unless x.between?(0, @x) or y.between?(0, @y)
-    @inner_representation[x][y]
+  def [](x_coord, y_coord)
+    raise "OutOfIndexError" unless x_coord.between?(0, @x_size) or y_coord.between?(0, @y_size)
+    @inner_representation[x_coord][y_coord]
   end
 
-  def []=(x,y,value)
-    raise "OutOfIndexError" unless x.between?(0, @x) or y.between?(0, @y)
-    @inner_representation[x][y]= value
+  def []=(x_coord,y_coord,value)
+    raise "OutOfIndexError" unless x_coord.between?(0, @x_size) or y_coord.between?(0, @y_size)
+    @inner_representation[x_coord][y_coord]= value
   end
 
   def clear!
-    @inner_representation = Array.new(@x) {|index| Array.new(@y, 0)}
+    @inner_representation = Array.new(@x_size) {|index| Array.new(@y_size, 0)}
   end
   
   def dup
-    copy = Board.new(@x, @y)
-    self.each_with_keys do |x, y, value|
-      copy[x,y] = value
+    copy = Board.new(@x_size, @y_size)
+    self.each_with_keys do |x_coord, y_coord, value|
+      copy[x_coord,y_coord] = value
     end
     copy
   end
