4112c2b0db1d944b46ce28b9d0f416c4a942a774
DarkTatka <darktatka@gmail.com>
2009-04-19 13:48:39 +0200

    prevod cest k souborum na konstanty jiz mohou hrat 2 GUI hraci na jedne desce(konecne)

--- lib/views/mig_mang_board.rb
+++ lib/views/mig_mang_board.rb
@@ -5,13 +5,16 @@ module View
     SQUARE_SIDE = 60
     FULL_SIZE = 8*SQUARE_SIDE
     FILE_PATH = File.dirname(__FILE__)
+    DESK_PATH = "/themes/simple.png"
+    PIECES_PATH = "/themes/pieces.png"
+    HIGHLIGHT_PATH = "/themes/composed-highlight.png"
 
     def initialize(parent = nil)
       super
       @theme = Theme.new
-      @theme.load_squares(FILE_PATH + "/themes/simple.png")
-      @theme.load_pieces(FILE_PATH + "/themes/pieces.png")
-      @theme.load_highlights(FILE_PATH + "/themes/composed-highlight.png")
+      @theme.load_squares(FILE_PATH + DESK_PATH)
+      @theme.load_pieces(FILE_PATH + PIECES_PATH)
+      @theme.load_highlights(FILE_PATH + HIGHLIGHT_PATH)
 
       @place_highlight = []
       @remove_highlight = []
@@ -64,19 +67,36 @@ module View
       when Qt::LeftButton:
         tile_x = event.x / SQUARE_SIDE
         tile_y = 8 - (event.y / SQUARE_SIDE)
-        
-        @place_highlight = @remove_highlight = @destination_highlight = []
-        
-        @moves = @board.moves_for(@board.on_move)
-        
-        moves_begining_at([tile_x,tile_y], @moves).each do |move|
-          highlight_move(move)
+
+        case @destination_highlight.include? [tile_x, tile_y]
+        when true:
+          move = select_move(@board.to_noted(@origin_highlight), @board.to_noted(tile_x, tile_y))
+          @board.apply_move!(move)
+          dehighlight
+        when false:
+          highliht_moves_from(tile_x, tile_y)
         end
         update
       end
     end
 
 private
+    def dehighlight
+      @origin_highlight = @place_highlight = @remove_highlight = @destination_highlight = []
+    end
+
+    def select_move(from, to)
+      move = @moves.find{|move| (move.first[1] == from) and (move[1][1] == to)}
+    end
+
+    def highliht_moves_from(tile_x, tile_y)
+      dehighlight
+      @moves = @board.moves_for(@board.on_move)
+      moves_begining_at([tile_x,tile_y], @moves).each do |move|
+        highlight_move(move)
+      end
+    end
+
     def moves_begining_at(position, moves)
       moves.find_all do |move|
         move.first[1] == @board.to_noted(position)
@@ -84,6 +104,7 @@ private
     end
 
     def highlight_move(move)
+      @origin_highlight = @board.from_noted(move.first[1])
       @place_highlight += move.find_all{|sub_move| sub_move.first == :place}.map{|sub_move| @board.from_noted(sub_move[1])}
       @remove_highlight += move.tail.find_all{|sub_move| sub_move.first == :remove}.map{|sub_move| @board.from_noted(sub_move[1])}
       @destination_highlight = @place_highlight #DANGER - zalozeno na pravidlech migmangu
