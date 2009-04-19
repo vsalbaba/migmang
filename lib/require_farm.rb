fd32c5ff9684c03d0332f60890090229ca217c5f
DarkTatka <darktatka@gmail.com>
2009-04-19 18:07:32 +0200

    refaktor velkych metod na male (do 6ti radku)

--- lib/require_farm.rb
+++ lib/require_farm.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'Qt4'
 
 this_file_path = File.dirname(__FILE__)
-
+require File.join(File.expand_path(this_file_path), "constant_farm")
 require File.join(File.expand_path(this_file_path), "views/mig_mang_board")
 require File.join(File.expand_path(this_file_path), "views/theme")
 require File.join(File.expand_path(this_file_path), "models/board")
@@ -12,8 +12,8 @@ require File.join(File.expand_path(this_file_path), "enhancements/array_enhancem
 require File.join(File.expand_path(this_file_path), "enhancements/fixnum_enhancements")
 require File.join(File.expand_path(this_file_path), "enhancements/move_enhancements")
 require File.join(File.expand_path(this_file_path), "enhancements/mig_mang_board_helper")
+require File.join(File.expand_path(this_file_path), "models/player/abstract_player")
 require File.join(File.expand_path(this_file_path), "models/player/minimax_player")
 require File.join(File.expand_path(this_file_path), "models/player/gui_player")
 require File.join(File.expand_path(this_file_path), "models/historian")
 require File.join(File.expand_path(this_file_path), "models/manager")
-require File.join(File.expand_path(this_file_path), "constant_farm")
--- lib/views/mig_mang_board.rb
+++ lib/views/mig_mang_board.rb
@@ -1,9 +1,10 @@
 module View
   class Board < Qt::Widget
     attr_accessor :board
-    SQUARES = 81
+    SQUARES_PER_SIDE = (MAX_SIZE+1)
+    SQUARES = SQUARES_PER_SIDE*SQUARES_PER_SIDE
     SQUARE_SIDE = 60
-    FULL_SIZE = 8*SQUARE_SIDE
+    FULL_SIZE = MAX_SIZE*SQUARE_SIDE
     FILE_PATH = File.dirname(__FILE__)
     DESK_PATH = "/themes/simple.png"
     PIECES_PATH = "/themes/pieces.png"
@@ -22,43 +23,11 @@ module View
     end
 
     def paintEvent(event)
-      puts "painting"
       painter = Qt::Painter.new(self)
       SQUARES.times do |square|
-        board_x = square % 9
-        board_y = square / 9
-        pixmap = case board_x
-        when 0:
-          case board_y
-          when 0: @theme.left_bottom
-          when 8: @theme.left_top
-          else @theme.left
-          end
-        when 8:
-          case board_y
-          when 0: @theme.right_bottom
-          when 8: @theme.right_top
-          else @theme.right
-          end
-        else
-          case board_y
-          when 0: @theme.bottom
-          when 8: @theme.top
-          else @theme.center
-          end
-        end
-        actual_x = board_x*SQUARE_SIDE
-        actual_y = FULL_SIZE - board_y*SQUARE_SIDE
-        painter.drawPixmap(actual_x, actual_y, pixmap)
-        unless @board[board_x,board_y].empty?
-          painter.drawPixmap(actual_x, actual_y, @theme.piece[board[board_x,board_y] -1])
-        end
-        if @destination_highlight.include?([board_x,board_y]) then
-          painter.drawPixmap(actual_x, actual_y, @theme.place_highlight)
-        end
-        if @remove_highlight.include?([board_x,board_y]) then
-          painter.drawPixmap(actual_x, actual_y, @theme.remove_highlight)
-        end
+        board_x, board_y = get_tile_coords(square)
+        actual_x, actual_y = get_actual_position(board_x, board_y)
+        paint_game_things_at([board_x, board_y], [actual_x, actual_y], painter)
       end
     end
 
@@ -74,13 +43,77 @@ module View
           @board.apply_move!(move)
           dehighlight
         when false:
-          highliht_moves_from(tile_x, tile_y)
+          highlight_moves_from(tile_x, tile_y)
         end
         update
       end
     end
 
 private
+    def paint_board_at(tile_coord, actual_coord, painter)
+      pixmap = select_board_picture_for(tile_coord)
+      painter.drawPixmap(actual_coord.first, actual_coord.last, pixmap)
+    end
+
+    def paint_game_things_at(tile_coord, actual_coord, painter)
+      paint_board_at(tile_coord, actual_coord, painter)
+      paint_pieces_at(tile_coord, actual_coord, painter)
+      paint_destination_highlight_at(tile_coord, actual_coord, painter)
+      paint_remove_highlight_at(tile_coord, actual_coord, painter)
+    end
+
+    def get_tile_coords(square_number)
+      [square_number % SQUARES_PER_SIDE, square_number / SQUARES_PER_SIDE]
+    end
+
+    def get_actual_position(tile_x, tile_y)
+      [tile_x*SQUARE_SIDE, FULL_SIZE - tile_y*SQUARE_SIDE]
+    end
+
+    def paint_remove_highlight_at(tile_coord, actual_coord, painter)
+      if @remove_highlight.include?(tile_coord) then
+        painter.drawPixmap(actual_coord.first, actual_coord.last, @theme.remove_highlight)
+      end
+    end
+
+    def paint_pieces_at(tile_coord, actual_coord, painter)
+      piece = @board[tile_coord.first,tile_coord.last]
+      unless piece.empty?
+        painter.drawPixmap(actual_coord.first, actual_coord.last, @theme.piece[piece -1])
+      end
+    end
+
+    def paint_destination_highlight_at(tile_coord, actual_coord, painter)
+      if @destination_highlight.include?(tile_coord) then
+        painter.drawPixmap(actual_coord.first, actual_coord.last, @theme.place_highlight)
+      end
+    end
+
+    def select_board_picture_for(tile_coord)
+      tile_x = tile_coord.first
+      tile_y = tile_coord.last
+      case tile_x
+      when 0:
+        case tile_y
+        when 0: @theme.left_bottom
+        when 8: @theme.left_top
+        else @theme.left
+        end
+      when 8:
+        case tile_y
+        when 0: @theme.right_bottom
+        when 8: @theme.right_top
+        else @theme.right
+        end
+      else
+        case tile_y
+        when 0: @theme.bottom
+        when 8: @theme.top
+        else @theme.center
+        end
+      end
+    end
+
     def dehighlight
       @origin_highlight = @place_highlight = @remove_highlight = @destination_highlight = []
     end
@@ -89,7 +122,7 @@ private
       move = @moves.find{|move| (move.first[1] == from) and (move[1][1] == to)}
     end
 
-    def highliht_moves_from(tile_x, tile_y)
+    def highlight_moves_from(tile_x, tile_y)
       dehighlight
       @moves = @board.moves_for(@board.on_move)
       moves_begining_at([tile_x,tile_y], @moves).each do |move|
@@ -105,8 +138,8 @@ private
 
     def highlight_move(move)
       @origin_highlight = @board.from_noted(move.first[1])
-      @place_highlight += move.find_all{|sub_move| sub_move.first == :place}.map{|sub_move| @board.from_noted(sub_move[1])}
-      @remove_highlight += move.tail.find_all{|sub_move| sub_move.first == :remove}.map{|sub_move| @board.from_noted(sub_move[1])}
+      @place_highlight += move.find_all{|place_sub_move| place_sub_move.first == :place}.map{|place_sub_move| @board.from_noted(place_sub_move[1])}
+      @remove_highlight += move.tail.find_all{|remove_sub_move| remove_sub_move.first == :remove}.map{|remove_sub_move| @board.from_noted(remove_sub_move[1])}
       @destination_highlight = @place_highlight #DANGER - zalozeno na pravidlech migmangu
     end
   end
